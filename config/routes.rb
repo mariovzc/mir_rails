d34211dffc41594c68156fa09fbda04d51021ddd
¨Mario <mario.vizcainos@gmail.com>
2017-09-12 22:28:38 -0500

    created products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+
+  resources :products, except: [:show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
