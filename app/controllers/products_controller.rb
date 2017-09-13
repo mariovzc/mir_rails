8102215e96d1aac5ce8dee5b7214930711a62580
¨Mario <mario.vizcainos@gmail.com>
2017-09-12 23:11:03 -0500

    added product categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,23 @@
 class ProductsController < ApplicationController
+  before_action :set_categories, only: [:new, :edit]
   def index
   end
 
   def new
+    @product = Product.new
+  end
+  def create
+    asd
   end
 
   def edit
+  end
+  private
+  def set_categories
+    @categories = Category.all
+  end
+  def product_params
+    params.require(:product).permit(:name, :price, @categories)
+    
   end
 end
