91a9757bc0628610ba146aa975fe8468cd138a26
¨Mario <mario.vizcainos@gmail.com>
2017-09-25 18:26:51 -0500

    fixed methods in the app

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,43 +1,40 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:edit,:update, :destroy]
   def index
-    @products = Product.all()
-    # pro3.categorys.pluck(:name).join(',')
-    # @products = CategoryProduct.joins(:product,:category).select('products.id','products.name as productos','categories.name as categorias')
+    @products = Product.all
   end
 
   def new
     @product = Product.new
     @categories = Category.all
     @product.categories.new
-    # @pro_cat = @product.categories.new
-    # @category_products = @product.categorys
   end
 
   def create
     @product = Product.new(product_params)
-    # @product.categories.new()
-    @product.save
-    redirect_to '/products'
+    if @product.save      
+      redirect_to root_path      
+    else
+      render :new
+    end
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
-    @product.update(product_params)
-    redirect_to '/products'
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
-    protected
-    def product_params
-      # debugger
-      params.require(:product).permit(:name, :price, category_ids: [])
-    end
+  protected
+  def set_product
+    @product = Product.find(params[:id])
+  end
+  def product_params
+    params.require(:product).permit(:name, :price, category_ids: [])
+  end
 end
-
-# select products.id,products.name,categories.name
-# from category_products
-# inner join products on products.id = category_products.product_id
-# inner join categories on categories.id = category_products.category_id
