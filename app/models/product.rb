f7dedd91e0ee7168e476126ff5a6037fb4c92543
¨Mario <mario.vizcainos@gmail.com>
2017-09-25 17:35:53 -0500

    update of models relations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,5 @@
 class Product < ApplicationRecord
-  has_many :product_categories, dependent: :destroy
-  has_many :categories,
+  has_and_belongs_to_many :categories,
     :through => :product_categories
   
-  accepts_nested_attributes_for :product_categories,
-    :allow_destroy => true
 end
